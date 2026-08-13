5bb6dc43a41206e4df4a47288425d0770c9c4a8a
Naut <meadonix@gmail.com>
2026-08-13 14:10:36 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.owner = [
 
 global.info = {
  nomerbot: '212605784394',
- pairingNumber: '',
+ pairingNumber: '50493059810',
  nameown: 'Nour',
  nomerown: '212717457920',
  packname: 'sticker by ',
